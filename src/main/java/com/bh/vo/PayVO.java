bc966f65967bb605f9e46efc875cbb48ad54dd93
~imooc~ <baihaoup@163.com>
2019-11-01 16:03:24 +0800

    11.1

---
+++ src/main/java/com/bh/vo/PayVO.java
@@ -0,0 +1,31 @@
+package com.bh.vo;
+
+public class PayVO {
+
+    private Long orderNo;
+    private String qrcode;
+
+    public PayVO() {
+    }
+
+    public PayVO(Long orderNo, String qrcode) {
+        this.orderNo = orderNo;
+        this.qrcode = qrcode;
+    }
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getQrcode() {
+        return qrcode;
+    }
+
+    public void setQrcode(String qrcode) {
+        this.qrcode = qrcode;
+    }
+}
